129be15ae4a19234e3e82cd3feecae58ce863085
John-Philip Johansson <john-philip.johansson@episerver.com>
2018-12-10 13:27:58 +0100

    Use the new data-epi-edit attribute
    This attribute was created to make it easier to get On-Page Editing for
    client-side rendered websites. Setting `data-epi-edit="PropertyName"` is
    equivalent to setting these three properties:
    * data-epi-property-name="PropertyName"
    * data-epi-property-render="none"
    * data-epi-property-edittype="floating"
    
    All three are otherwise needed when doing client-side rendering so the CMS
    doesn't update the DOM and break its connection to the client-side rendering
    framework.
    
    Task: CMS-12661
    Feature: CMS-12641

--- src/MusicFestival.Vue.Template/Assets/Scripts/directives/epiEdit.js
+++ src/MusicFestival.Vue.Template/Assets/Scripts/directives/epiEdit.js
@@ -1,42 +1,26 @@
 /**
  * The directive `v-epi-edit` is used similarly to @Html.EditAttributes() in
- * Razor views. It enables On-Page Editing on elements and disables the DOM
+ * Razor views. It enables On-Page Editing on elements using the `data-epi-edit`
+ * property (introduced in Episerver CMS UI 11.X.0) and disables the DOM
  * updating from the CMS so that Vue can keep the responsibility over the DOM.
  *
- * It's enabled by the `isEditable` value that is stored in the vuex store, but
+ * It's enabled by the `isEditable` value that is stored in the Vuex store, but
  * can be overwritten by a component having a property named
  * `epiDisableEditing` being true.
- *
- * Set's the needed data-epi attributes on an element:
- * - data-epi-property-name="[the given property name]"
- * - data-epi-property-render="none"
- * - data-epi-property-edittype="floating"
  *
  * Usage can be found on most Vue components, such as ArtistDetailsPage.vue.
  */
 
 import store from '@/Scripts/store';
 
-function removeEditAttributes(el) {
-    el.removeAttribute('data-epi-property-name');
-    el.removeAttribute('data-epi-property-render');
-    el.removeAttribute('data-epi-property-edittype');
-}
-
-function setEditAttributes(el, binding) {
-    el.setAttribute('data-epi-property-name', binding.value);
-    el.setAttribute('data-epi-property-render', 'none');
-    el.setAttribute('data-epi-property-edittype', 'floating');
-}
-
 function toggleEditAttributes(el, binding, vnode) {
     const siteIsEditable = store.state.epiContext.isEditable;
     const componentIsEditable = !vnode.context.epiDisableEditing;
 
     if (siteIsEditable && componentIsEditable) {
-        setEditAttributes(el, binding);
+        el.setAttribute('data-epi-edit', binding.value);
     } else {
-        removeEditAttributes(el);
+        el.removeAttribute('data-epi-edit');
     }
 }
 
